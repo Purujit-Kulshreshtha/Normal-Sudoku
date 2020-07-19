5d88a2177d2b1d753acaa67658ce5eb12dfc0824
Purujit-Kulshreshtha <purujitkulshreshtha8@gmail.com>
2020-07-19 15:16:12 +0530

    added button class

--- App_class.py
+++ App_class.py
@@ -1,51 +1,72 @@
 import pygame
 import sys
 from Settings import * 
+from ButtonClass import *
 
+#create app class
 class App:
 	def __init__(self):
 		pygame.init()
 		self.window = pygame.display.set_mode((WIDTH, HEIGHT))
-		self.running = True
+		self.running = True #game variable
 		self.board = BOARD
-		self.selected = None
-		self.mouse_pos = None
+		self.selected = None #box selection variable
+		self.mouse_pos = None 
+		self.state = "playing" #the state of the game(menu, playing, ended etc.)
+		#buttons for each state
+		self.playing_buttons = []
+		self.menu_buttons = []
+		self.end_buttons = []
+		self.load_buttons()
 
 	def run(self):
 		while self.running:
-			self.events()
-			self.update()
-			self.draw()
+			if self.state == "playing":
+				self.playing_events()
+				self.playing_update()
+				self.playing_draw()
 		pygame.quit()
 		sys.exit()
 
-	def events(self):
+	#### Playing-state Functions ###
+
+	def playing_events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				self.running = False
+				self.running = False #to quit
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				selected = self.mouse_on_grid(self.mouse_pos)
+				selected = self.mouse_on_grid(self.mouse_pos) #get position of selected box
 				if selected:
-					self.selected = selected
+					self.selected = selected #store               ^
 				else:
 					print("Mouse not on grid.")
 					self.selected = None
 
-	def update(self):
-		self.mouse_pos = pygame.mouse.get_pos()
-		
+	def playing_update(self):
+		self.mouse_pos = pygame.mouse.get_pos() #get postition of mouse at every frame
 
-	def draw(self):
-		self.window.fill(BG)
-		if self.selected:
+		for button in self.playing_buttons:
+			button.update(self.mouse_pos)
+
+		
+	def playing_draw(self):
+		self.window.fill(BG) #main window
+
+		for button in self.playing_buttons:
+			button.draw(self.window)
+			print("Button Drawn")
+
+		if self.selected: #fill selected box
 			self.draw_selection(self.window, self.selected)
-		self.draw_grid(self.window)
+		self.draw_grid(self.window) #draw grid
 		pygame.display.update()
 
+	### Helper Functions ###
+
 	def draw_grid(self, window):
-		pygame.draw.rect(window, WHITE, GRID_TRANS, 2)
-		for x in range(0, 9):
-			if x % 3 != 0:
+		pygame.draw.rect(window, WHITE, GRID_TRANS, 2) #draw grid box
+		for x in range(0, 9): #draw grid lines
+			if x % 3 != 0: 
 				pygame.draw.line(window, WHITE, (GRID_TRANS[0] + x*(CELL_SIZE), GRID_TRANS[1]), (GRID_TRANS[0] + x*(CELL_SIZE), GRID_TRANS[1] + 450)) #vertical lines
 				pygame.draw.line(window, WHITE, (GRID_TRANS[0], GRID_TRANS[1] + x*(CELL_SIZE)), (GRID_TRANS[0] + 450, GRID_TRANS[1] + x*(CELL_SIZE))) # horizontal lines
 			else:
@@ -53,13 +74,14 @@ class App:
 				pygame.draw.line(window, WHITE, (GRID_TRANS[0], GRID_TRANS[1] + x*(CELL_SIZE)), (GRID_TRANS[0] + 450, GRID_TRANS[1] + x*(CELL_SIZE)), 4) #horizontal lines bold
 
 	def mouse_on_grid(self, mouse_pos):
-		in_grid = [False, False]
+		in_grid = [False, False] 
 		if mouse_pos[0] >= grid_pos[0] and mouse_pos[0] <= grid_pos[0] + GRID_SIZE:
 			in_grid[0] = True
 		if mouse_pos[1] >= grid_pos[1] and mouse_pos[1] <= grid_pos[1] + GRID_SIZE:
 			in_grid[1] = True
 		if in_grid[0] == True and in_grid[1] == True:
 			mouse_pos = list(mouse_pos)
+			#manipulate position values as required
 			mouse_pos[0] -= grid_pos[0]
 			mouse_pos[0] = mouse_pos[0]//CELL_SIZE
 			mouse_pos[1] -= grid_pos[1]
@@ -69,6 +91,8 @@ class App:
 		else:
 			return False
 
-	def draw_selection(self, window, pos):
+	def draw_selection(self, window, pos): #fill selected box function
 		pygame.draw.rect(window, CO_1, ((pos[0]*CELL_SIZE)+grid_pos[0], ( pos[1]*CELL_SIZE)+grid_pos[1],  CELL_SIZE, CELL_SIZE))
 
+	def load_buttons(self):
+		self.playing_buttons.append(Button(20, 40, 100, 40))
---
+++ ButtonClass.py
@@ -0,0 +1,28 @@
+import pygame
+from Settings import *
+
+class Button:
+	def __init__(self, x, y, width, height, text=None, color=WHITE, high_color=CO_1, function=None, params=None):
+		self.image = pygame.Surface((width, height))
+		self.pos = (x, y)
+		self.rect = self.image.get_rect()
+		self.rect.topleft = self.pos
+		self.text = text
+		self.color = color
+		self.high_color = high_color
+		self.function = function
+		self.params = params
+		self.highlighted = False
+
+	def update(self, mouse):
+		if self.rect.collidepoint(mouse):
+			self.highlighted = True
+		else:
+			self.highlighted = False
+
+	def draw(self, window):
+		if self.highlighted:
+			self.image.fill(self.high_color)
+		else:
+			self.image.fill(self.color)
+		window.blit(self.image, self.pos)
